a016c1d00c710bb7339498dc4da5e567d4976ce8
julio.arguello <julio.arguello@63f8c964-9ab9-11de-9e41-ffeadb3e0f14>
2009-12-26 18:54:27 +0000

    Code checked with Checkstyle

--- bluebell-richclient/src/test/java/org/bluebell/richclient/application/looks/substance/TestSubstanceLookAndFeelConfigurer.java
+++ bluebell-richclient/src/test/java/org/bluebell/richclient/application/looks/substance/TestSubstanceLookAndFeelConfigurer.java
@@ -1,20 +1,22 @@
 /**
  * 
  */
-package org.bluebell.richclient.application.looks.substance;
+package org.bluebell.richclient.application.docking.vldocking.substance;
 
 import org.junit.Test;
 
 /**
  * @author julio.arguello
  * 
- *        TODO
+ *         TODO
  */
 public class TestSubstanceLookAndFeelConfigurer {
 
-    
+    /**
+     * TODO.
+     */
     @Test
     public void todo() {
-	
+
     }
 }
